71bfc3cd580fbe1b7da4a44729290ddba779a1f1
Niall Brennan <niallbrennan87@gmail.com>
2017-03-18 17:14:13 +0000

    working

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -11,17 +11,26 @@
 
     $scope.countItems = function () {
       var items = $scope.list.split(',');
+      var realItems = 0;
       console.log(items);
+      for (var i = 0; i < items.length; i++) {
+        if (items[i]) {
+          realItems++
+        }
+      }
       var amount = items.length;
       console.log(amount);
+      console.log(realItems);
       displayMessage(amount)
     }
 
     function displayMessage(amount) {
-      if (amount <= 3) {
-        $scope.message = "yum";
+      if (amount == 0) {
+        $scope.message = "Please enter data first";
+      } else if (amount <= 3) {
+        $scope.message = "Enjoy!";
       } else {
-        $scope.message = "yuk";
+        $scope.message = "Too much!";
       }
     }
 
