81bc11633db94e87c412f31db23d058a3a02e1ff
Niall Brennan <niallbrennan87@gmail.com>
2017-04-09 15:18:55 +0100

    cat working

---
+++ module4-solution/src/menudata.service.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+
+angular.module('Data')
+.service('MenuDataService', MenuDataService)
+.constant('Cat', "https://davids-restaurant.herokuapp.com/categories.json")
+.constant('ItemForCat', "https://davids-restaurant.herokuapp.com/menu_items.json");
+
+MenuDataService.$inject = ['$http', 'Cat', 'ItemForCat']
+function MenuDataService($http, Cat, ItemForCat) {
+  var service = this;
+
+  // Returns a promise, NOT items array directly
+  service.getAllCategories = function () {
+    return $http({
+      method: "GET",
+      url: "https://davids-restaurant.herokuapp.com/categories.json"
+    });
+  };
+
+  service.getItemsForCategory = function (categoryShortName) {
+    return $http({
+        method: "GET",
+        url: ItemForCat,
+        params: {category: categoryShortName}
+      })
+  };
+}
+
+})();
---
+++ module4-solution/src/routes.js
@@ -0,0 +1,46 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/templates/home.template.html'
+  })
+
+
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/templates/categories.template.html',
+    controller: 'CategoriesController as catgCtrl',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  .state('items', {
+    url: '/items/{itemId}',
+    templateUrl: 'src/templates/item.template.html',
+    controller: 'ItemController as ItemCtrl',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+}
+
+})();
