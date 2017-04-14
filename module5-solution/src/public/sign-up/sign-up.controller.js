a5d22cc79bb07fc37bb59d6652e3588722f2bc25
Niall Brennan <niallbrennan87@gmail.com>
2017-04-14 16:13:42 +0100

    finished

--- module5-solution/src/public/sign-up/sign-up.controller.js
+++ module5-solution/src/public/sign-up/sign-up.controller.js
@@ -13,45 +13,15 @@ function SignUpController(MenuService) {
 
     if (regForm.favourite.value) {
       MenuService.getItem(regForm.favourite.value).then(function (response) {
-        MenuService.setFav(regForm.favourite.value);
-        reg.valid = true;
+        MenuService.setFav(response);
+        reg.valid = "Your information has been saved";
       })
       .catch(function (errorResponse){
-        MenuService.setFav('Not set');
-        reg.valid = false;
+        MenuService.setFav(false);
+        reg.valid = "No such menu number exists";
       })
     }
 
-
   }
-
 }
-
 })();
-
-
-// (function () {
-//   "use strict";
-//
-//   angular.module('public')
-//   .controller('signUpController', signUpController);
-//
-//   signUpController.$inject = ['ApiPath', 'MenuService'];
-//   function signUpController(ApiPath, MenuService)
-//   {
-//     var $ctrl = this;
-//     $ctrl.submit = function () {
-//       $ctrl.basePath = ApiPath;
-//
-//       MenuService.getItem(regForm.favoriteDish.value).then(function () {
-//         MenuService.setInfo(regForm);
-//         $ctrl.valid = true;
-//         $ctrl.wrongDish = false;
-//       })
-//       .catch(function (errorResponse){
-//         $ctrl.valid = false;
-//         $ctrl.wrongDish = true;
-//       })
-//     }
-//   }
-// })();
